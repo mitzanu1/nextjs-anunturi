495046f464144cd29f631ffb2f61444b915271bf
mitzanu <mitzanu@hotmail.com>
2021-08-17 16:43:31 +0300

    test

---
+++ events/[...slug].js
@@ -0,0 +1,109 @@
+import { useRouter } from "next/router";
+import React, { Fragment } from "react";
+import EventList from "../components/events/event-list";
+import ResultsTitle from "../components/events/results-title";
+import Button from "../components/ui/button";
+import ErrorAlert from "../components/ui/error-alert";
+import { getAnunturi } from "../api_utils/dbApi";
+
+const EventSlug = (props) => {
+  const router = useRouter();
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading ...</p>;
+  }
+  const filterdYear = +filterData[0];
+  const filterdMonth = +filterData[1];
+
+  if (props.hasError) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values.</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const filterdEvents = props.events.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === filterdYear &&
+      eventDate.getMonth() === filterdMonth - 1
+    );
+  });
+
+  if (!filterdEvents || filterdEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found for the chosen filter.</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(filterdYear, filterdMonth - 1);
+  return (
+    <Fragment>
+      <ResultsTitle date={date} />
+      <EventList items={filterdEvents} />
+    </Fragment>
+  );
+};
+
+export default EventSlug;
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  var res = await fetch("https://nextjs-anunturi.vercel.app/api/anunt", {
+    method: "GET",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "User-Agent": "*",
+    },
+  });
+
+  const data = await res.json();
+
+  const filterData = params.slug;
+  const filterdYear = +filterData[0];
+  const filterdMonth = +filterData[1];
+
+  if (
+    isNaN(filterdYear) ||
+    isNaN(filterdMonth) ||
+    filterdYear > 2030 ||
+    filterdYear < 2021 ||
+    filterdMonth < 1 ||
+    filterdMonth > 12
+  ) {
+    return {
+      props: { hasError: true },
+      // notFound:true,
+      // redirect: {
+      //   destination: '/error'
+      // }
+    };
+  }
+
+  const filterdEvents = data.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === filterdYear &&
+      eventDate.getMonth() === filterdMonth - 1
+    );
+  });
+  return {
+    props: {
+      events: filterdEvents,
+    },
+  };
+}
---
+++ events/[eventsId].js
@@ -0,0 +1,86 @@
+import React, { Fragment, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import EventSummary from "../components/event-detail/event-summary";
+import EventLogistics from "../components/event-detail/event-logistics";
+import EventContent from "../components/event-detail/event-content";
+import ErrorAlert from "../components/ui/error-alert";
+import Button from "../components/ui/button";
+import { getAnunturi } from "../api_utils/dbApi";
+
+const EventDetail = (props) => {
+  const router = useRouter();
+  const eventId = router.query.eventsId;
+  const event = props.events.filter((event) => event._id === eventId)[0];
+  const { title, updatedAt, contact, image, description, price, city } = event;
+
+  if (!event) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No event found</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={title} />
+      <EventLogistics
+        date={updatedAt}
+        tel={contact}
+        image={image}
+        imageAlt={title}
+        price={price}
+        city={city}
+      />
+      <EventContent>
+        <p>{description}</p>
+      </EventContent>
+    </Fragment>
+  );
+};
+
+export default EventDetail;
+
+export async function getStaticPaths() {
+  var res = await fetch("https://nextjs-anunturi.vercel.app/api/anunt", {
+    method: "GET",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "User-Agent": "*",
+    },
+  });
+
+  const data = await res.json();
+
+  const pathsArr = data.map((item) => ({
+    params: { eventsId: item._id },
+  }));
+  return {
+    paths: pathsArr,
+    fallback: "blocking",
+  };
+}
+
+export async function getStaticProps() {
+  var res = await fetch("https://nextjs-anunturi.vercel.app/api/anunt", {
+    method: "GET",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "User-Agent": "*",
+    },
+  });
+
+  const data = await res.json();
+
+  return {
+    props: {
+      events: data,
+      revalidate: 1,
+    },
+  };
+}
