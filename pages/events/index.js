ec6a4d38c3525de67c6039f8f090d911bc3c9c75
mitzanu <mitzanu@hotmail.com>
2021-08-17 17:04:57 +0300

    fix

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import React, { Fragment } from "react";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
-import { getAnunturi } from "../api_utils/dbApi";
+import { getAnunturi } from "../../api_utils/dbApi";
 
 const EventsPage = (props) => {
   const allEvents = props.events;
