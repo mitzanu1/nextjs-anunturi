5670730bdddd39d53fe267c04d32154457dccfef
mitzanu <mitzanu@hotmail.com>
2021-08-17 16:41:21 +0300

    test

--- api_utils/dbApi.js
+++ api_utils/dbApi.js
@@ -1,7 +1,8 @@
 export async function getAnunturi() {
   const localhost = "http://localhost:3000/api/anunt";
   const vercel = "https://nextjs-anunturi.vercel.app/api/anunt";
-  var res = await fetch(vercel, {
+
+  var res = await fetch("https://nextjs-anunturi.vercel.app/api/anunt", {
     method: "GET",
     headers: {
       Accept: "application/json, text/plain, */*",
