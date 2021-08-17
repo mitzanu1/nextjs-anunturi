44804aeeddf0c582c80912619f7d02b56a5648e4
mitzanu <mitzanu@hotmail.com>
2021-08-17 15:58:20 +0300

    test

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,8 @@
 import { useRouter } from "next/router";
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment } from "react";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { getAnunturi } from "../../api_utils/dbApi";
-import axios from "axios";
 
 const EventsPage = (props) => {
   const allEvents = props.events;
@@ -24,9 +23,6 @@ export default EventsPage;
 
 export async function getStaticProps() {
   const data = await getAnunturi();
-  // const { data } = await axios.get(
-  //   "https://nextjs-anunturi.vercel.app/api/getAnunt"
-  // );
 
   return {
     props: {
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import EventList from "../components/events/event-list";
 import { getAnunturi } from "../api_utils/dbApi";
-import axios from "axios";
 
 const HomePage = (props) => {
   const featuredEvents = props.events;
-  // const [featuredEvents, setFeaturedEvents] = useState([]);
 
-  // const getAndSet = async () => {
-  //   const res = await getAnunturi();
-  //   setFeaturedEvents(res);
-  // };
-  // console.log(featuredEvents);
-
-  // useEffect(() => {
-  //   getAndSet();
-  // }, []);
   return (
     <div>
       <EventList items={featuredEvents} />
@@ -27,10 +16,6 @@ export default HomePage;
 
 export async function getStaticProps() {
   const data = await getAnunturi();
-
-  // const { data } = await axios.get(
-  //   "https://nextjs-anunturi.vercel.app/api/getAnunt"
-  // );
 
   return {
     props: {
